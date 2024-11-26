70c20eaf2ba1a7d31db0263a5992bb977391bcc7
OscarMacchiavello <mapeoscar123@gmail.com>
2024-11-26 02:13:55 -0500

    new structure

---
+++ backend/vaccinationsys/dao/loteDAO.py
@@ -0,0 +1,18 @@
+from ..models import Lote
+from .dao_interface import DAOInterface
+
+class LoteDAO(DAOInterface):
+    def crear(self, data):
+        pass
+
+    def eliminar(self, id):
+        pass
+
+    def modificar(self, id, data):
+        pass
+
+    def obtener(self, id):
+        pass
+    
+    def obtenerTodos(self, data):
+        pass
